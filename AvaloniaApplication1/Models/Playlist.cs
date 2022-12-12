e10ed68debb42d6aeec78b5425b05222d71ae457
FravinCustodian <3114226@gmail.com>
2022-12-12 23:38:37 +0200

    Add project files.

---
+++ AvaloniaApplication1/Models/Playlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication1.Models
+{
+    public class Playlist
+    {
+        public List<Track> Tracks { get; set; }
+        public string Name { get; set; }
+
+        public string AvatarUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+using AvaloniaApplication1.Models;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<Track> list { get; set; }
+
+        public MainWindowViewModel()
+        {
+            list = new List<Track>();
+            for (int i = 0; i < 3; i++)
+            {
+                list.Add(new Track()
+                {
+                    Name = i.ToString()
+                });
+            }
+        }
+    }
+}
