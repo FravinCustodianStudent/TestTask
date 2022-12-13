d335120175b57886db3ee8f3561ea7c8c313bf58
FravinCustodian <3114226@gmail.com>
2022-12-13 09:52:51 +0200

    AntiDTEKcommit

--- AvaloniaApplication1/Services/PlaylistService.cs
+++ AvaloniaApplication1/Services/PlaylistService.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AvaloniaApplication1.Models;
+using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace AvaloniaApplication1.Services
 {
     public class PlaylistService
     {
+
+        public async Task<Playlist> GetPlaylist(string url)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            await Task.Delay(3000);
+            var page = driver.PageSource;
+            var elements = driver.FindElement(By.TagName("music-text-row"));
+
+            return null;
+        }
     }
 }
--- AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,33 @@
 ﻿
 using System.Collections.Generic;
 using AvaloniaApplication1.Models;
+using AvaloniaApplication1.Services;
 
 namespace AvaloniaApplication1.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public Playlist Playlist { get; set; }
         public List<Track> list { get; set; }
 
         public MainWindowViewModel()
         {
-            list = new List<Track>();
+            PlaylistService service= new PlaylistService();
+            var s = service.GetPlaylist(@"https://music.amazon.com/albums/B0BLH6NR6N");
+            Playlist = new Playlist();
+            Playlist.Name = "example";
+            Playlist.AvatarUrl = "url";
+            Playlist.Description = "description";
+            
+            Playlist.Tracks = new List<Track>();
             for (int i = 0; i < 3; i++)
             {
-                list.Add(new Track()
+                Playlist.Tracks.Add(new Track()
                 {
-                    Name = i.ToString()
+                    Name = i.ToString(),
+                    ArtistName = (i+2).ToString(),
+                    AlbumName = (i+3).ToString(),
+                    Duration = (i+4).ToString()
                 });
             }
         }
